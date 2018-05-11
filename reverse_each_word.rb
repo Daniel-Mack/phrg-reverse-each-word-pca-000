e0183432723fc688626a72734298862da2ecaebb
Daniel-Mack <daniel.mack@powerhrg.com>
2018-05-10 21:56:35 -0400

    Built reverse method

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+
+def reverse_each_word(sentence)
+  reversed_sentence = []
+  reversed_sentence << sentence.split.collect  do |word|
+   word.reverse
+ end
+ return reversed_sentence.join(" ")
+end
